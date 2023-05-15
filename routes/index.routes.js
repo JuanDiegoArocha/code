142999975aa2a9256ddc86237401215b8faef753
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-05-15 13:34:09 +0200

    error resuelto

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,14 +9,14 @@ router.get("/", (req, res, next) => {
 const authRouter = require('./auth.routes');
 router.use('/auth', authRouter); 
 
-<<<<<<< HEAD
+
 const productRouter = require('./product.routes');
 router.use('/product', productRouter);
 
 
-=======
+
 const profileRouter = require('./profile.routes');
 router.use('/user', profileRouter); 
->>>>>>> 2e33ae86509e6f967e596f164a313fe86709dff6
+
 
 module.exports = router;
