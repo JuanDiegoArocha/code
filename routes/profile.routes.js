5d036cb510ae23fa533db59d141cdb1a025210b8
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-05-16 13:34:12 +0200

    Edit profile added

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -43,6 +43,59 @@ router.get('/profile', isLoggedIn, (req, res, next) => {
     
 });
 
+// GET Edit Profile route
+router.get("/edit-profile", isLoggedIn, (req, res, next) => {
+    User.findById(req.session.user._id)
+    .then((user) => {
+
+        res.render("user/edit-profile.hbs", {
+            user,
+        })
+
+    })
+    .catch((err) => {
+        next (err)
+    })
+})
+
+
+// POST Edit Profile route
+router.post("/edit-profile", isLoggedIn, async (req, res, next) => {
+    try {
+
+        const userId = req.session.user._id;
+
+        const { firstName, 
+            lastName, 
+            email, 
+            address, 
+            city, 
+            postalCode, 
+            dateOfBirth
+         } = req.body;
+
+         const updatedUser = await User.findByIdAndUpdate(userId, {
+            firstName,
+            lastName,
+            email,
+            address,
+            city,
+            postalCode,
+            dateOfBirth
+         },
+         { new: true }
+         )
+         res.redirect("/user/profile")
+
+    } catch (error) {
+        next (error)
+    }
+
+
+})
+
+
+
 
 // GET Cart route
 router.get("/cart", isLoggedIn, async (req, res, next) => {
