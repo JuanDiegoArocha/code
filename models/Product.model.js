ad23f3b9158fc24a02f0e9d176ead62e9c1409e0
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-05-17 14:42:53 +0200

    push de prueba

--- models/Product.model.js
+++ models/Product.model.js
@@ -21,15 +21,14 @@ const productSchema = new Schema(
     type: Number,
     required: true,
   },
-<<<<<<< HEAD
+
   purchased: {
     type: Boolean,
     default: false,
   },
-  coverImage: String //! a traves de cloudinary porner las URL
-=======
+
   image: String //! a traves de cloudinary porner las URL
->>>>>>> 522df9e76fd3338d62b75cd009b14b65bdb9a09f
+
 },
   {
   //esto es para añadir propiedades : `createdAt` y `updateAt`
